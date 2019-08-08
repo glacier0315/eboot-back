be9fe41d7bff150f8481e2d326b764ca87d5c750
glacier <glacier0315@126.com>
2019-08-08 22:24:28 +0800

    添加角色管理

---
+++ eboot-server/src/main/java/com/glacier/sys/dao/RoleDao.java
@@ -0,0 +1,14 @@
+package com.glacier.sys.dao;
+
+import com.glacier.common.dao.CrudDao;
+import com.glacier.sys.entity.Role;
+
+/**
+ * @author glacier
+ * @version 1.0
+ * @description  角色dao层
+ * @date 2019-08-04 21:53
+ */
+public interface RoleDao extends CrudDao<Role, String> {
+
+}
---
+++ eboot-server/src/main/java/com/glacier/sys/entity/Role.java
@@ -0,0 +1,70 @@
+package com.glacier.sys.entity;
+
+import com.glacier.common.entity.BaseEntity;
+
+/**
+ * @author glacier
+ * @version 1.0
+ * @description  角色
+ * @date 2019-08-04 13:45
+ */
+public class Role extends BaseEntity {
+    private static final long serialVersionUID = -3318599726827564559L;
+    /**
+     * 角色名称
+     */
+    private String roleName;
+    /**
+     * 角色编码
+     */
+    private String roleCode;
+    /**
+     * 状态
+     */
+    private String status;
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName == null ? null : roleName.trim();
+    }
+
+    public String getRoleCode() {
+        return roleCode;
+    }
+
+    public void setRoleCode(String roleCode) {
+        this.roleCode = roleCode == null ? null : roleCode.trim();
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status == null ? null : status.trim();
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id='" + id + '\'' +
+                ", roleName='" + roleName + '\'' +
+                ", roleCode='" + roleCode + '\'' +
+                ", status='" + status + '\'' +
+                ", createBy='" + createBy + '\'' +
+                ", createDate=" + createDate +
+                ", updateBy='" + updateBy + '\'' +
+                ", updateDate=" + updateDate +
+                ", delFlag='" + delFlag + '\'' +
+                ", id='" + id + '\'' +
+                ", delFlag='" + delFlag + '\'' +
+                ", createBy='" + createBy + '\'' +
+                ", createDate=" + createDate +
+                ", updateBy='" + updateBy + '\'' +
+                ", updateDate=" + updateDate +
+                '}';
+    }
+}
