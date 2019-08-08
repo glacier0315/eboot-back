df08bbc6c1d1f6c8a949e9da9d7ae2af4e2f4f12
glacier <glacier0315@126.com>
2019-08-08 22:25:39 +0800

    添加角色管理

--- eboot-server/db/init.sql
+++ eboot-server/db/init.sql
@@ -1,4 +1,4 @@
-create table t_user
+create table if not exists t_user
 (
 	id varchar(64) not null comment '主键'
 		primary key,
@@ -13,5 +13,36 @@ create table t_user
 	update_by varchar(64) null comment '更新人',
 	update_date timestamp null comment '更新时间',
 	del_flag varchar(4) null comment '删除标记'
+);
+
+
+
+create table if not exists t_role
+(
+    id varchar(64) not null comment '主键'
+        primary key,
+    role_name varchar(200) null comment '角色名',
+    role_code varchar(255) null comment '角色编码',
+    status varchar(4) null comment '状态',
+    create_by varchar(64) null comment '创建人',
+    create_date timestamp null comment '创建时间',
+    update_by varchar(64) null comment '更新人',
+    update_date timestamp null comment '更新时间',
+    del_flag varchar(4) null comment '删除标记'
+);
+
+
+
+create table if not exists t_user_role
+(
+    id varchar(64) not null comment '主键'
+        primary key,
+    user_id varchar(200) null comment '用户主键',
+    role_id varchar(255) null comment '角色主键',
+    create_by varchar(64) null comment '创建人',
+    create_date timestamp null comment '创建时间',
+    update_by varchar(64) null comment '更新人',
+    update_date timestamp null comment '更新时间',
+    del_flag varchar(4) null comment '删除标记'
 );
 
