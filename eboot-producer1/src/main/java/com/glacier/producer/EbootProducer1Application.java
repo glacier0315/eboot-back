bc443db25e34e59f55e0074b771ec966379731c0
glacier <glacier0315@126.com>
2019-10-29 22:38:55 +0800

    添加服务消费者 producer

---
+++ eboot-producer1/src/main/java/com/glacier/producer/EbootProducer1Application.java
@@ -0,0 +1,20 @@
+package com.glacier.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author hebin
+ * @version 1.0
+ * @description
+ * @date 2019-10-29 17:40
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EbootProducer1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbootProducer1Application.class, args);
+    }
+}
---
+++ eboot-producer2/src/main/java/com/glacier/producer/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.glacier.producer.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author hebin
+ * @version 1.0
+ * @description
+ * @date 2019-10-29 17:44
+ */
+@RestController
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello() {
+        return "hello world 2";
+    }
+}
