0560240d826b7c9b4e57595c98c1225f6a8ab249
glacier <glacier0315@126.com>
2019-10-30 12:32:01 +0800

    添加feign 服务熔断

--- eboot-consumer/src/main/java/com/glacier/consumer/openfeign/service/EbootProducerService.java
+++ eboot-consumer/src/main/java/com/glacier/consumer/openfeign/service/EbootProducerService.java
@@ -1,5 +1,6 @@
 package com.glacier.consumer.openfeign.service;
 
+import com.glacier.consumer.openfeign.service.hystrix.EbootProducerHystrix;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @description
  * @date 2019-10-30 12:10
  */
-@FeignClient("eboot-producer")
+@FeignClient(name = "eboot-producer", fallback = EbootProducerHystrix.class)
 public interface EbootProducerService {
 
     @RequestMapping("/hello")
---
+++ eboot-consumer/src/main/java/com/glacier/consumer/openfeign/service/hystrix/EbootProducerHystrix.java
@@ -0,0 +1,19 @@
+package com.glacier.consumer.openfeign.service.hystrix;
+
+import com.glacier.consumer.openfeign.service.EbootProducerService;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author hebin
+ * @version 1.0
+ * @description
+ * @date 2019-10-30 12:21
+ */
+@Component
+public class EbootProducerHystrix implements EbootProducerService {
+
+    @Override
+    public String hello() {
+        return "sorry, hello service call failed.";
+    }
+}
