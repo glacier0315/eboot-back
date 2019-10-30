f806e635abf6074ad92fcbf3a4cefefe6d82254d
glacier <glacier0315@126.com>
2019-10-30 15:54:20 +0800

    添加网关

---
+++ eboot-zuul/src/main/java/com/glacier/zuul/EbootZuulApplication.java
@@ -0,0 +1,20 @@
+package com.glacier.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author hebin
+ * @version 1.0
+ * @description
+ * @date 2019-10-30 15:05
+ */
+@EnableZuulProxy
+@SpringBootApplication
+public class EbootZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbootZuulApplication.class, args);
+    }
+}
---
+++ eboot-zuul/src/main/java/com/glacier/zuul/filter/MyPreFilter.java
@@ -0,0 +1,73 @@
+package com.glacier.zuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author hebin
+ * @version 1.0
+ * @description
+ * @date 2019-10-30 15:06
+ */
+@Component
+@Slf4j
+public class MyPreFilter extends ZuulFilter {
+    /**
+     * 定义filter的类型，有pre、route、post、error四种
+     *
+     * @return
+     */
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    /**
+     * 定义filter的顺序，数字越小表示顺序越高，越先执行
+     *
+     * @return
+     */
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    /**
+     * 表示是否需要执行该filter，true表示执行，false表示不执行
+     *
+     * @return
+     */
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    /**
+     * filter需要执行的具体操作
+     *
+     * @return
+     * @throws ZuulException
+     */
+    @Override
+    public Object run() throws ZuulException {
+//        RequestContext ctx = RequestContext.getCurrentContext();
+//        HttpServletRequest request = ctx.getRequest();
+//        String token = request.getParameter("token");
+//        System.out.println(token);
+//        if (token == null) {
+//            log.warn("there is no request token");
+//            ctx.setSendZuulResponse(false);
+//            ctx.setResponseStatusCode(401);
+//            try {
+//                ctx.getResponse().getWriter().write("there is no request token");
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//            return null;
+//        }
+//        log.info("ok");
+        return null;
+    }
+}
