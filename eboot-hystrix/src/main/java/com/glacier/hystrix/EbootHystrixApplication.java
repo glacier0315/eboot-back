5c3037ae88d6c902c304fafcb10cd4e13b506b4b
glacier <glacier0315@126.com>
2019-10-30 13:20:31 +0800

    添加hystrix监控

--- eboot-hystrix/src/main/java/com/glacier/hystrix/EbootHystrixApplication.java
+++ eboot-hystrix/src/main/java/com/glacier/hystrix/EbootHystrixApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,6 +15,7 @@ import org.springframework.web.client.RestTemplate;
  * @description
  * @date 2019-10-29 17:40
  */
+@EnableTurbine
 @EnableHystrixDashboard
 @EnableDiscoveryClient
 @SpringBootApplication
