98876f94ff466bbb09a890fdceea22544df7de0f
glacier <glacier0315@126.com>
2019-12-18 17:13:31 +0800

    修改表名

--- db/init.sql
+++ db/init.sql
@@ -1,4 +1,4 @@
-create table t_user (
+create table sys_user (
 		id varchar ( 64 ) not null comment '主键' primary key,
 		username varchar ( 200 ) comment '用户名',
 		password varchar ( 255 ) comment '密码',
@@ -17,12 +17,12 @@ create table t_user (
 		update_date timestamp null comment '更新时间',
 		del_flag varchar ( 4 ) comment '删除标记'
 	) comment '用户表';
-create index t_user_1 on t_user ( username );
-create index t_user_2 on t_user ( id_card );
-create index t_user_3 on t_user ( status );
+create index sys_user_1 on sys_user ( username );
+create index sys_user_2 on sys_user ( id_card );
+create index sys_user_3 on sys_user ( status );
 
 
-create table t_role (
+create table sys_role (
 	id varchar ( 64 ) not null comment '主键' primary key,
 	name varchar ( 200 ) comment '角色名',
 	code varchar ( 255 ) comment '角色编码',
@@ -33,20 +33,20 @@ create table t_role (
 	update_date timestamp null comment '更新时间',
 	del_flag varchar ( 4 ) comment '删除标记'
 ) comment '角色表';
-create index t_role_1 on t_role ( code );
-create index t_role_2 on t_role ( status );
+create index sys_role_1 on sys_role ( code );
+create index sys_role_2 on sys_role ( status );
 
 
-create table t_user_role (
+create table sys_user_role (
 	id varchar ( 64 ) not null comment '主键' primary key,
 	user_id varchar ( 64 ) comment '用户主键',
 	role_id varchar ( 64 ) comment '角色主键'
 ) comment '用户角色关联表';
-create index t_user_role_1 on t_user_role ( user_id );
-create index t_user_role_2 on t_user_role ( role_id );
+create index sys_user_role_1 on sys_user_role ( user_id );
+create index sys_user_role_2 on sys_user_role ( role_id );
 
 
-create table t_menu (
+create table sys_menu (
 	id varchar ( 64 ) not null comment '主键' primary key,
 	name varchar ( 200 ) comment '名称',
 	url varchar ( 255 ) comment 'url',
@@ -63,21 +63,21 @@ create table t_menu (
 	update_date timestamp null comment '更新时间',
 	del_flag varchar ( 4 ) comment '删除标记'
 ) comment '菜单表';
-create index t_menu_1 on t_menu ( parent_id );
-create index t_menu_2 on t_menu ( type );
-create index t_menu_3 on t_menu ( status );
+create index sys_menu_1 on sys_menu ( parent_id );
+create index sys_menu_2 on sys_menu ( type );
+create index sys_menu_3 on sys_menu ( status );
 
 
-create table t_role_menu (
+create table sys_role_menu (
 	id varchar ( 64 ) not null comment '主键' primary key,
 	role_id varchar ( 255 ) comment '角色主键',
 	menu_id varchar ( 255 ) comment '菜单主键'
 ) comment '角色菜单关联表';
-create index t_role_menu_1 on t_role_menu ( menu_id );
-create index t_role_menu_2 on t_role_menu ( role_id );
+create index sys_role_menu_1 on sys_role_menu ( menu_id );
+create index sys_role_menu_2 on sys_role_menu ( role_id );
 
 
-create table t_dept (
+create table sys_dept (
 	id varchar ( 64 ) not null comment '主键' primary key,
 	name varchar ( 200 ) comment '名称',
 	code varchar ( 255 ) comment '编码',
@@ -92,24 +92,24 @@ create table t_dept (
 	update_date timestamp null comment '更新时间',
 	del_flag varchar ( 4 ) comment '删除标记'
 ) comment '组织机构表';
-create index t_dept_1 on t_dept ( parent_id );
-create index t_dept_2 on t_dept ( type );
-create index t_dept_3 on t_dept ( status );
-create index t_dept_4 on t_dept ( code );
+create index sys_dept_1 on sys_dept ( parent_id );
+create index sys_dept_2 on sys_dept ( type );
+create index sys_dept_3 on sys_dept ( status );
+create index sys_dept_4 on sys_dept ( code );
 
 
 
-create table t_role_dept (
+create table sys_role_dept (
 	id varchar ( 64 ) not null comment '主键' primary key,
 	role_id varchar ( 255 ) comment '角色主键',
 	dept_id varchar ( 255 ) comment '组织机构主键'
 ) comment '角色和组织机构关联表';
-create index t_role_dept_1 on t_role_dept ( dept_id );
-create index t_role_dept_2 on t_role_dept ( role_id );
+create index sys_role_dept_1 on sys_role_dept ( dept_id );
+create index sys_role_dept_2 on sys_role_dept ( role_id );
 
 
 
-create table t_config (
+create table sys_config (
 	id varchar ( 64 ) not null comment '主键' primary key,
 	value varchar ( 255 ) comment '值',
 	label varchar ( 255 ) comment '名称',
@@ -123,11 +123,11 @@ create table t_config (
 	update_date timestamp null comment '更新时间',
 	del_flag varchar ( 4 ) comment '删除标记'
 ) comment '设置表';
-create index t_config_1 on t_config ( type );
-create index t_config_2 on t_config ( value );
+create index sys_config_1 on sys_config ( type );
+create index sys_config_2 on sys_config ( value );
 
 
-create table t_dict (
+create table sys_dict (
 	id varchar ( 64 ) not null comment '主键' primary key,
 	code varchar ( 50 ) comment '值',
 	name varchar ( 50 ) comment '名称',
@@ -142,6 +142,27 @@ create table t_dict (
 	update_date timestamp null comment '更新时间',
 	del_flag varchar ( 4 ) comment '删除标记'
 ) comment '字典表';
-create index t_dict_1 on t_dict ( type );
-create index t_dict_2 on t_dict ( code );
-create index t_dict_3 on t_dict ( parent_id );
+create index sys_dict_1 on sys_dict ( type );
+create index sys_dict_2 on sys_dict ( code );
+create index sys_dict_3 on sys_dict ( parent_id );
+
+create table sys_log (
+	id varchar ( 64 ) not null comment '主键' primary key,
+	user_id varchar ( 64 ) comment '登录id',
+	url varchar ( 255 ) comment 'url',
+	ip varchar ( 255 ) comment '登录ip',
+	user_agent varchar ( 255 ) comment '登录客户端',
+    use_time int ( 10 ) comment '耗时微秒'
+) comment '操作日志表';
+create index sys_log_1 on sys_log ( user_id );
+
+
+create table sys_login_log (
+	id varchar ( 64 ) not null comment '主键' primary key,
+	user_id varchar ( 64 ) comment '登录id',
+	ip varchar ( 255 ) comment '登录ip',
+	user_agent varchar ( 255 ) comment '登录客户端'
+) comment '登录日志表';
+create index sys_login_log_1 on sys_login_log ( user_id );
+
+
