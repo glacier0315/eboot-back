89a3b3f29e18b25b8448ffd3ad9a58beacf0a49c
glacier <glacier0315@126.com>
2020-02-12 09:55:30 +0800

    添加资源服务器

---
+++ eboot-auth/eboot-auth-resource/src/main/java/com/glacier/auth/config/FeignConfig.java
@@ -0,0 +1,26 @@
+package com.glacier.auth.config;
+
+import feign.Logger;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author glacier
+ * @version 1.0
+ * @description Feign配置
+ * @date 2020-02-10 19:33
+ */
+@Configuration
+@EnableFeignClients("com.glacier.auth.consumer")
+public class FeignConfig {
+
+    /**
+     * 修改Feign日志输出级别
+     * @return
+     */
+    @Bean
+    Logger.Level feignLevel() {
+        return Logger.Level.FULL;
+    }
+}
